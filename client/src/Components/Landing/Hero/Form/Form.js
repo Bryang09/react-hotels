9a6070e1949021781035fef25d52951a393374d5
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-02-26 18:37:44 -0600

    Form Logic Added

--- client/src/Components/Landing/Hero/Form/Form.js
+++ client/src/Components/Landing/Hero/Form/Form.js
@@ -1,24 +1,62 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./Form.scss";
 
-class Form extends Component {
-  render() {
-    return (
-      <div className="Form">
-        <h1>Find the Perfect Hotel Today!</h1>
+const Form = props => {
+  const {
+    cityOptions,
+    cityChange,
+    roomOptions,
+    roomChange,
+    nameChange,
+    emailChange,
+    disable
+  } = props;
 
-        <form>
-          <select name="city">
-            <option selected="selected" />
-            <option value="Houston">Houston</option>
-            <option value="Dallas">Dallas</option>
-            <option value="Austin">Austin</option>
-          </select>
-        </form>
-      </div>
+  const cityValues = cityOptions.map(res => {
+    return (
+      <option key={res} value={res}>
+        {res}
+      </option>
     );
-  }
-}
+  });
+
+  const roomValues = roomOptions.map(res => {
+    return (
+      <option value={res} key={res}>
+        {res}
+      </option>
+    );
+  });
+
+  // console.log(cityOptions);
+  // console.log(cityValues);
+  // console.log(roomOptions);
+  // console.log(roomValues);
+
+  return (
+    <div className="Form">
+      <h1>Find the Perfect Hotel Today!</h1>
+      <form>
+        <div className="row1">
+          <input type="text" placeholder="Name" onChange={nameChange} />
+          <input type="email" placeholder="Email" onChange={emailChange} />
+        </div>
+
+        <div className="row2">
+          <select name="city" onChange={cityChange}>
+            <option defaultValue />
+            {cityValues}
+          </select>
+          <select name="rooms" onChange={roomChange}>
+            <option defaultValue />
+            {roomValues}
+          </select>
+        </div>
+      </form>
+      <button disabled={!disable}>Search Hotels</button>
+    </div>
+  );
+};
 
 export default Form;
--- client/src/Components/Landing/Landing.js
+++ client/src/Components/Landing/Landing.js
@@ -11,7 +11,11 @@ import "./Landing.scss";
 
 class Landing extends Component {
   state = {
-    results: []
+    results: [],
+    city: "",
+    rooms: "",
+    name: "",
+    email: ""
   };
 
   componentWillMount = () => {
@@ -21,14 +25,58 @@ class Landing extends Component {
       .catch(err => console.log(err));
   };
 
+  onCity = e => {
+    e.preventDefault();
+    this.setState({ city: e.target.value });
+  };
+
+  onRooms = e => {
+    e.preventDefault();
+    this.setState({ rooms: e.target.value });
+  };
+
+  onName = e => {
+    e.preventDefault();
+    this.setState({ name: e.target.value });
+  };
+
+  onEmail = e => {
+    e.preventDefault();
+    this.setState({ email: e.target.value });
+  };
+
   render() {
+    const { results, city, rooms, name, email } = this.state;
+
+    const check =
+      this.state.name.length > 0 &&
+      this.state.email.length > 0 &&
+      this.state.rooms.length > 0 &&
+      this.state.city.length > 0;
+
     console.log(results);
-    console.log(this.state.results);
+    // console.log(this.state.results);
+    // console.log(this.state.city);
+    // console.log(this.state.rooms);
+    // console.log(this.state.name);
+    // console.log(this.state.email);
+    console.log(check);
+
+    const cityOptions = ["Houston", "Dallas", "Austin"];
+    const roomOptions = [1, 2, 3, 4, 5];
 
     return (
       <div className="Landing">
         <Img />
-        <Form />
+        <Form
+          cityChange={this.onCity}
+          cityOptions={cityOptions}
+          roomChange={this.onRooms}
+          roomOptions={roomOptions}
+          nameChange={this.onName}
+          emailChange={this.onEmail}
+          disable={check}
+        />
       </div>
     );
   }
